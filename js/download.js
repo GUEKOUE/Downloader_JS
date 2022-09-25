fb106ad63970b706555fdb4d0a61be0771ca4af5
GUEKOUE <compassiondemenou@yahoo.com>
2022-09-25 22:49:15 +0100

    initial commit

---
+++ js/download.js
@@ -0,0 +1,20 @@
+const bar = document.querySelector('.bar');
+
+let width = 1;
+const success = document.querySelector('.success');
+const counter = document.querySelector('.counter');
+
+function start() {
+    setInterval(() => {
+        if (width >= 100){
+            success.innerHTML = 'Download completed!';
+            success.style.opacity = 1;
+            clearInterval();
+        }
+        else{
+            width++;
+            bar.style.width = width + '%';
+            counter.innerHTML = width + '%';
+        }
+    }, 10);
+}
